158d9d482b8ef1cd55a92838e57030b7f3cba37a
Kenechukwu <kenechukwu.ajufo@gmail.com>
2021-12-16 03:15:20 +0000

    Add New Content

--- script.js
+++ script.js
@@ -24,18 +24,18 @@ const checkbox = document.getElementById('toggle');
 // }());
 
 if (hour <= 7 || hour >= 18) {
-    document.getElementById('pagestyle').setAttribute('href', "dark.css");
+    document.getElementById('pagestyle').setAttribute('href', "/styles/dark.css");
 }
 else {
-    document.getElementById('pagestyle').setAttribute('href', "style.css");
+    document.getElementById('pagestyle').setAttribute('href', "/styles/style.css");
     document.getElementById("toggle").checked = false;
 }
 
 checkbox.addEventListener('change', (event) => {
   if (event.target.checked) {
-    document.getElementById('pagestyle').setAttribute('href', "dark.css");
+    document.getElementById('pagestyle').setAttribute('href', "/styles/dark.css");
   } else {
-    document.getElementById('pagestyle').setAttribute('href', "style.css");
+    document.getElementById('pagestyle').setAttribute('href', "/styles/style.css");
   }
 })
 
@@ -60,7 +60,7 @@ const hand = document.getElementById("emoji");
 
 var typed = new Typed('.element', {
     // Waits 1000ms after typing "First"
-    strings: ['everything related to web development.', 'building Recommender Systems.', 'Product Management.', 'Machine Learning.'],
+    strings: ['everything related to web development.', 'Machine Learning.', "Business Intelligence."],
     typeSpeed: 100,
     loop: true,
     loopCount: Infinity,
@@ -82,17 +82,3 @@ var typed = new Typed('.element', {
     }
   );
   wow.init();
-
-
-var arr = [
-    { "Name": "Sentiment Analysis of Hotel Reviews using Deep Learning", "Link": "https://github.com/kxnxchukwu/DMML2Project", 'Description': '(Team of three) performed a sentiment analysis on Trip Advisor Reviews dataset. The project focused on using two Deep Learning models (LSTM and BERT) to predict the hotel’s rating using it’s reviews.', 'Tech': 'scikit-learn | TensorFlow | PyTorch | Keras'},
-    { "Name": "Analysis on the Relationship between Population Growth Rate, Life Expectancy and the Gross National Income", "Link": "https://github.com/kxnxchukwu/DAPProject", 'Description': '(Team of three) analyzed three public datasets from ArcGIS Hub & Datahub. The objective of the project was to demonstrate the team’s knowledge of data wrangling (transforming data from one form to another) and Relational (PostgreSQL) & NoSQL (MongoDB) databases.', 'Tech': 'pymongo | psycopg2 | Pandas | requests'},
-    { "Name": "Exploring Student Academic Performance in Portugal using Machine Learning", "Link": "https://github.com/kxnxchukwu/DAPAProject", 'Description': 'Built a C5.0 Decision Tree Model to predict student academic performance.', 'Tech': 'R | R Studio | C50 | printr'},
-    { "Name": "London Underground High-Speed Line Simulation and Optimization", "Link": "https://github.com/kxnxchukwu/MSOProject", 'Description': 'The project involved discrete event simulation and optimization. The project modelled, simulated and optimized the London Old Oak Commons to Birmingham Interchange section of the London Underground high-speed line.', 'Tech': 'Python | SimPy | Pandas | Numpy'},
-  ];
-
-$.each(arr, function (i) {
-    var templateString = '<div style="margin:20px" class="card col-lg-5 mx-auto col-sm-12" style="width: 18rem"> <div class="card-body"> <div class="card-title d-flex"><i class="fas fa-folder-open fa-3x mr-auto"></i>  <a href='+ arr[i].Link + '><i class="fab fa-github fa-3x ml-auto"></i></a> </div> <h6 class="card-subtitle mt-2 mb-2 text-muted">' + arr[i].Name +  '</h6> <p class="card-text">' + arr[i].Description + '</p> <p class="card-text text-muted">' + arr[i].Tech + '</p></div></div> ';
-    $('#test').append(templateString);
-})
-
